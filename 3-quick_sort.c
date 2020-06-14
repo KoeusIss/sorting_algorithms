a779ca8fb54a3ec2224c1a6e03f35dbaed6ef3b1
KoeusIss <sebri.issam@gmail.com>
2020-06-14 13:56:11 +0100

    commit the qiuck sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two integers
+ * @first: first integer
+ * @second: second integer
+ *
+ * Return: void
+ */
+void swap(int *first, int *second)
+{
+	int tmp;
+
+	tmp = *first;
+	*first = *second;
+	*second = tmp;
+}
+
+/**
+ * partition - partitions an array seperate by pivot
+ * @array: the given array
+ * @low: the lowest value of the array
+ * @high: the highest value of the array
+ *
+ * Return: the pivot position as integer
+ */
+int partition(int *array, int low, int high)
+{
+	int pivot = high;
+	int i = low;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= array[pivot])
+		{
+			swap(&array[i], &array[j]);
+			i++;
+		}
+	}
+	swap(&array[i], &array[high]);
+	return (i);
+}
+
+/**
+ * quick_sort_lumoto - sorts recursively an array of integer
+ * @array: the given array of integer
+ * @low: the lowest value of the array
+ * @high: the highest value of the array
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort_lumoto(int *array, int low, int high, const size_t size)
+{
+	size_t prt;
+
+	if (low < high)
+	{
+		prt = partition(array, low, high);
+		print_array(array, size);
+		quick_sort_lumoto(array, low, prt - 1, size);
+		quick_sort_lumoto(array, prt + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers
+ * @array: the given array of integers
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_lumoto(array, 0, size - 1, size);
+}
